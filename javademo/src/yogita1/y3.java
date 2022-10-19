8719bbaeb969ea6f0fcbcd539bcfb090c0dd423c
91935 <91935@Yogita>
2022-10-19 16:32:42 +0530

    first commit

---
+++ javademo/src/yogita1/y3.java
@@ -0,0 +1,13 @@
+package yogita1;
+
+public class y3 {
+	public static void main(String[] args) {
+		boolean var = false;
+		System.out.println(var);
+
+		char var1 = '\u00A7';
+		System.out.println(var1);
+		
+	}
+
+}
