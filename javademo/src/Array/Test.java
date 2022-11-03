4bd6d0be386b1220740f5a12ef8eee34dba474cf
91935 <91935@Yogita>
2022-11-03 15:41:45 +0530

    first commit

---
+++ javademo/src/Array/Test.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class Test {
+
+	public static void main(String[] args) {
+
+				int arr[]=new int[5];
+				arr[0]=2;
+				arr[1]=4;
+				arr[2]=6;
+				arr[3]=6;
+				arr[4]=8;
+				for(int i:arr)//for each
+				{
+					System.out.println(i);
+				}
+
+			}
+
+		}
+
+
+
+
+
