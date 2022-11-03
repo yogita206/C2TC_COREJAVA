dd3fd040b38263f635f1d9c0cf049d0df2ea9d5a
91935 <91935@Yogita>
2022-11-03 15:42:34 +0530

    first commit

---
+++ javademo/src/Array/D2.java
@@ -0,0 +1,17 @@
+package Array;
+
+public class D2 {
+	public static void main(String[] args) {
+		
+		int arr[][]= {{2,4,6},{3,5,7}};
+	for(int i=0;i<2;i++)
+	{
+		for(int j=0;j<3;j++)
+		{
+			System.out.print(arr[i][j]+" ");
+		}
+		System.out.println();
+	}
+	}
+
+}
